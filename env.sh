24296604ee3ebb6acd6bb6ebc65e098f243b6445
Gurban <palwanishan@seoultech.ac.kr>
2021-11-17 19:21:34 +0900

    Environment files
    Environment shell files to be created:
    env.sh -> inside ~/BBB/ folder
    uEnv.txt -> inside fs/rootfs/ folder
    uEnv.txt -> inside fs/rootfs/boot folder
    build folder -> ~/BBB/ folder

---
+++ env.sh
@@ -0,0 +1,7 @@
+export WORK_DIR=$PWD
+export PATH=$PATH:$WORK_DIR/toolchain/gcc-linaro-arm-linux-gnueabihf-4.7-2013.04-20130415_linux/bin
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export ARCH=arm
+export LINUX_TREE=$WORK_DIR/kernel/linux
+export ROOTFS=$WORK_DIR/fs/rootfs
+export BUILD_ROOT=$WORK_DIR/build
